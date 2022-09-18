cfa88631ef2496baa61c7581aeabf060d652f4a9
davwhite <davwhite@davwhite.com>
2022-09-18 12:51:20 -0400

    Upload results.csv

--- app/trainit.sh
+++ app/trainit.sh
@@ -16,3 +16,7 @@ $ARTI_REPO/$TRAINING_NAME/$TRAINING_VER/$TRAINING_NAME.pt
 curl -v -u $ARTI_USER:$ARTI_PWD \
 --upload-file /usr/local/lib/python3.9/site-packages/yolov5/data/$MODEL_CLASSES \
 $ARTI_REPO/$TRAINING_NAME/$TRAINING_VER/$MODEL_CLASSES
+
+curl -v -u $ARTI_USER:$ARTI_PWD \
+--upload-file /usr/local/lib/python3.9/site-packages/yolov5/runs/train/exp/results.csv \
+$ARTI_REPO/$TRAINING_NAME/$TRAINING_VER/results.csv
